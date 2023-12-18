c48e27a74ba2d84ea3bea9145223a6e7fa905677
root <root@DESKTOP-K4B994M>
2023-12-17 20:03:59 -0800

    sixth commit

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -4,9 +4,21 @@
 const AboutUs= () => {
     return(
         <div>
-            <h1>About Us</h1>
-            <h3></h3>
-        </div>
+            <center><h1>About Us</h1>
+
+            <h3> Read Me Library's mission is to provide qualtiy reading materials to children and adults of all ages. With our comprehensive literacy program, we provide
+                services to people of all ages weekly. We conduct outreach with our esteemed Read Me Book Bus, that travels to surrounding neighborhoods bringing the love of reading! 
+                Check our schedule as the Read Me Book Bus may be heading to a neighborhood near you!!
+                <ul>
+                    <h2> Hours of Operation:</h2>
+                    <li>
+                        Monday - Sunday
+                    </li>
+                    <li>
+                       7:00am -8:00pm
+                    </li>
+                    </ul></h3>
+            </center></div>
     )
 
 }
